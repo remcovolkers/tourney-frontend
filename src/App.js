aa109b431bf1bc4e362b1aae3c4f65aecf205fd8
Remco Volkers <460318@student.saxion.nl>
2022-10-13 22:58:10 +0200

    init + centeralized textbox

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import LoginScreen from "./components/LoginScreen";
+import "./styles/App.scss";
+
+function App() {
+  return <LoginScreen></LoginScreen>;
+}
+
+export default App;
---
+++ src/components/LoginScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginScreen = () => {
+  return <div className="login-container">loginscreen</div>;
+};
+
+export default LoginScreen;
